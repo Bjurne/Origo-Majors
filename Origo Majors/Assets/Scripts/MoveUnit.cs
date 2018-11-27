c198181bee33a3d45631289cee2e3267e6db5d3e
Kristoffer Svedlund <kristoffersvedlund@gmail.com>
2018-11-27 12:14:58 +0100

    Unity crashed, rebuilt my test scene

---
+++ Origo Majors/Assets/Scripts/MoveUnit.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveUnit : MonoBehaviour {
+    public GameObject unitToMove;
+
+	void Start () {
+		
+	}
+	
+	
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            unitToMove = this.gameObject;
+            unitToMove.transform.position = (Input.mousePosition);
+        }
+    }
+}
