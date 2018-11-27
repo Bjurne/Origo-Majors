e9244ac469556d315e824721514cdcf5a0608618
Kristoffer Svedlund <kristoffersvedlund@gmail.com>
2018-11-27 14:03:18 +0100

    Slow progress

--- Origo Majors/Assets/Scripts/MoveUnit.cs
+++ Origo Majors/Assets/Scripts/MoveUnit.cs
@@ -1,15 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class MoveUnit : MonoBehaviour {
+public class MoveUnit : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHandler {
 
-	void Start () {
-		
+    RaycastHit hit;
+
+
+	public void OnBeginDrag (PointerEventData eventData) {
+        Debug.Log("OnBeginDrag");
 	}
-	
-	
-	void Update () {
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        Debug.Log("OnBeginDrag");
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        Debug.Log("OnBeginDrag");
+    }
+
+
+    void Update () {
         if (Input.touchCount > 0)
         {
             Touch touch = Input.GetTouch(0);
@@ -20,9 +35,15 @@ public class MoveUnit : MonoBehaviour {
 
         if (Input.GetMouseButtonDown(0))
         {
-            Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            mousePosition.z = 0f;
-            transform.position = mousePosition;
+            
+            if (Physics.Raycast(Camera.main.transform.position, Input.mousePosition, out hit))
+            {
+                Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                mousePosition.z = 0f;
+                transform.position = mousePosition;
+            }
         }
+
+        
     }
 }
