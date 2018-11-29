e8289e77b82974b6f588ebefd7796b6ac0f230f8
Kristoffer Svedlund <kristoffersvedlund@gmail.com>
2018-11-29 11:45:03 +0100

    Pretty useless code for click movement, but at least it works.

--- Origo Majors/Assets/Scripts/MoveUnit.cs
+++ Origo Majors/Assets/Scripts/MoveUnit.cs
@@ -2,48 +2,38 @@
 using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
-using UnityEngine.EventSystems;
 
-public class MoveUnit : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHandler {
+public class MoveUnit : MonoBehaviour {
 
-    RaycastHit hit;
+    //RaycastHit hit;
+    float distance = 10;
 
-
-	public void OnBeginDrag (PointerEventData eventData) {
-        Debug.Log("OnBeginDrag");
-	}
-
-    public void OnDrag(PointerEventData eventData)
-    {
-        Debug.Log("OnBeginDrag");
-    }
-
-    public void OnEndDrag(PointerEventData eventData)
-    {
-        Debug.Log("OnBeginDrag");
-    }
+    //void Update()
+    //{
+    //    Vector3 mousePosition = new Vector3 (Input.mousePosition.x, Input.mousePosition.y, Input.mousePosition.z);
+    //    Vector3 objPosition = Camera.main.ScreenToWorldPoint(mousePosition);
+    //    this.transform.position = objPosition;
+    //}
 
 
-    void Update () {
-        if (Input.touchCount > 0)
-        {
-            Touch touch = Input.GetTouch(0);
-            Vector3 touchPosition = Camera.main.ScreenToWorldPoint(touch.position);
-            touchPosition.z = 0f;
-            transform.position = touchPosition;
-        }
+    //void Update () {
+    //    if (Input.touchCount > 0)
+    //    {
+    //        Touch touch = Input.GetTouch(0);
+    //        Vector3 touchPosition = Camera.main.ScreenToWorldPoint(touch.position);
+    //        touchPosition.z = 0f;
+    //        transform.position = touchPosition;
+    //    }
 
-        if (Input.GetMouseButtonDown(0))
-        {
-            
-            if (Physics.Raycast(Camera.main.transform.position, Input.mousePosition, out hit))
-            {
-                Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-                mousePosition.z = 0f;
-                transform.position = mousePosition;
-            }
-        }
+    //    if (Input.GetMouseButtonDown(0))
+    //    {
 
-        
-    }
+    //        if (Physics.Raycast(Camera.main.transform.position, Input.mousePosition, out hit))
+    //        {
+    //            Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    //            mousePosition.z = 0f;
+    //            transform.position = mousePosition;
+    //        }
+    //    }
+    //}
 }
---
+++ Origo Majors/Assets/clickListener.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class clickListener : MonoBehaviour {
+
+    public LayerMask selectable;
+    public LayerMask waypoints;
+    public GameObject currentlySelectedObject = null;
+    public GameObject selectionMarker = null;
+    public GameObject selectedWaypoint = null;
+
+
+    void Update ()
+    {
+        if (Input.GetMouseButtonDown (0))
+        {
+
+            Vector3 clickPosition = -Vector3.one;
+            Plane plane = new Plane(Vector3.up, 0f);
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            float distanceToPlane;
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, 1000f, waypoints) && currentlySelectedObject != null)
+            // och ifall selected waypoint = legal waypoint destination
+            {
+                clickPosition = hit.point;
+                selectedWaypoint = hit.collider.gameObject;
+
+                currentlySelectedObject.transform.position = selectedWaypoint.transform.position;
+
+                Debug.Log(currentlySelectedObject.name + " has been moved to " + selectedWaypoint.name);
+
+                selectionMarker.SetActive(false);
+                currentlySelectedObject = null;
+                selectionMarker = null;
+                selectedWaypoint = null;
+            }
+
+            if (currentlySelectedObject != null)
+            {
+                selectionMarker.SetActive(false);
+                currentlySelectedObject = null;
+                selectionMarker = null;
+                selectedWaypoint = null;
+            }
+
+            if (Physics.Raycast(ray, out hit, 1000f, selectable))
+            {
+                clickPosition = hit.point;
+                currentlySelectedObject = hit.rigidbody.gameObject;
+                selectionMarker = currentlySelectedObject.transform.GetChild(0).gameObject;
+                selectionMarker.SetActive(true);
+                if (currentlySelectedObject != null)
+                {
+                    Debug.Log("Currently selected object is " + currentlySelectedObject.name);
+                    Debug.Log("The Selection Marker of Currently selected object is " + selectionMarker.name);
+                }
+            }
+            else
+            {
+                //currentlySelectedObject.transform.GetChild(0).gameObject.SetActive(false);
+                //currentlySelectedObject = null;
+                Debug.Log("Currently selected object is none");
+                Debug.Log("The Selection Marker of Currently selected object is none");
+            }
+
+            if (plane.Raycast(ray, out distanceToPlane))
+            {
+                clickPosition = ray.GetPoint(distanceToPlane);
+            }
+
+            Debug.Log(clickPosition);
+        }
+    }
+
+    //void SetSelectedObject()
+    //{
+
+    //}
+}
