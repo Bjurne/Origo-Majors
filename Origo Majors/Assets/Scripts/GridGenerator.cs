84a214f9be9c08600308953eb29b052200f86f34
Anton <a.e.lindkvist@gmail.com>
2018-12-03 09:42:47 +0100

    Back at it again at krispy kreme

---
+++ Origo Majors/Assets/Scripts/GridGenerator.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridGenerator : MonoBehaviour {
+
+
+
+    private void Awake ()
+    {
+        
+    }
+
+    private void Start () {
+		
+	}
+	
+
+	void Update () {
+		
+	}
+}
