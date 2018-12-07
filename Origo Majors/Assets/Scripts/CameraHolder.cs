358f1421c582a088a8b56c5d66aa774ae7a4d273
Anton <a.e.lindkvist@gmail.com>
2018-12-07 15:46:13 +0100

    Added automatic camera input (todo: manual input)

--- Origo Majors/Assets/Scripts/CameraHolder.cs
+++ Origo Majors/Assets/Scripts/CameraHolder.cs
@@ -4,13 +4,34 @@ using UnityEngine;
 
 public class CameraHolder : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public Camera myCamera;
+    public GridGenerator gridGenerator;
+    GridNode tempGridNode;
+    Vector3 turnSpeed = new Vector3(0, 100, 0);
+    Vector3 cameraOffset = new Vector3(0, -7, 0);
+
+    void Start ()
+    {
+        myCamera.transform.LookAt(transform.position + cameraOffset);
+        transform.position = Thingy();
+    }
+
+    void Update ()
+    {
+        transform.Rotate(turnSpeed * Time.deltaTime);
+    }
+
+    public Vector3 Thingy ()
+    {
+        Vector3 cameraHeight = new Vector3(0, 0, 0);
+        GridNode testNode;
+        if (gridGenerator.dic.TryGetValue(gridGenerator.FindCenter(), out testNode))
+        {
+            return testNode.gameObject.transform.position + cameraHeight;
+        }
+        Debug.Log("Debug!");
+        return Vector3.zero;
+    }
+
+
 }
