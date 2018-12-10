b272bd4c133d25e989a4715fc4098a7e1ec3c97d
Anton <a.e.lindkvist@gmail.com>
2018-12-10 15:25:34 +0100

    Added beginning of a new drag-input method for camera movement.

--- Origo Majors/Assets/Scripts/CameraHolder.cs
+++ Origo Majors/Assets/Scripts/CameraHolder.cs
@@ -9,6 +9,18 @@ public class CameraHolder : MonoBehaviour {
 
     public Vector3 turnSpeed = new Vector3(0, 100, 0);
     Vector3 cameraOffset = new Vector3(0, -7, 0);
+    
+    //Temp controller
+    float x;
+
+    public float dragSpeed = 2;
+    private Vector3 dragOrigin;
+
+    public bool cameraDragging = true;
+
+    public float outerLeft = -10f;
+    public float outerRight = 10f;
+
 
     void Start ()
     {
@@ -18,7 +30,16 @@ public class CameraHolder : MonoBehaviour {
 
     void Update ()
     {
-        transform.Rotate(turnSpeed * Time.deltaTime);
+
+        CameraDrag();
+
+        //Regular ol button-input rotation
+        if (Input.GetKey("a") || Input.GetKey("d"))
+        {
+            x = Input.GetAxisRaw("Horizontal");
+            CameraRotation(x);
+        }
+        //IdleRotation();
     }
 
     public Vector3 MoveToCenter ()
@@ -31,6 +52,63 @@ public class CameraHolder : MonoBehaviour {
         }
         Debug.Log("Debug!");
         return Vector3.zero;
+    }
+
+    void IdleRotation ()
+    {
+        transform.Rotate(turnSpeed * Time.deltaTime);
+    }
+
+    void CameraRotation (float x)
+    {
+        transform.Rotate(0, x, 0);
+    }
+
+    void CameraDrag ()
+    {
+        Vector2 mousePosition = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+
+        float left = Screen.width * 0.2f;
+        float right = Screen.width - (Screen.width * 0.2f);
+
+        if (mousePosition.x < left)
+        {
+            cameraDragging = true;
+        }
+        else if (mousePosition.x > right)
+        {
+            cameraDragging = true;
+        }
+
+        if (cameraDragging)
+        {
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                dragOrigin = Input.mousePosition;
+                return;
+            }
+
+            if (!Input.GetMouseButton(0)) return;
+
+            Vector3 pos = Camera.main.ScreenToViewportPoint(Input.mousePosition - dragOrigin);
+            Vector3 move = new Vector3(0, pos.x * dragSpeed, 0);
+
+            if (move.x > 0f)
+            {
+                if (this.transform.position.x < outerRight)
+                {
+                    transform.Rotate(move);
+                }
+            }
+            else
+            {
+                if (this.transform.position.x > outerLeft)
+                {
+                    transform.Rotate(move);
+                }
+            }
+        }
     }
 
 
