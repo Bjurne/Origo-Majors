387bb99b7e3e9f217fad7947de33ac585b67fe21
Kristoffer Svedlund <kristoffersvedlund@gmail.com>
2018-12-11 15:13:40 +0100

    Building a simple Victory Screen

---
+++ Origo Majors/Assets/VictoryScreenScript.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Linq;
+
+public class VictoryScreenScript : MonoBehaviour {
+
+    public StateManager stateManager;
+    public Text firstPlaceText;
+    public Text secondPlaceText;
+    public Text thirdPlaceText;
+    public Text fourthPlaceText;
+    public string winnerName;
+
+    //void DisplayVictoryScreen () {
+    void Start ()
+    {
+        int blueScore = stateManager.blueScore;
+        int redScore = stateManager.redScore;
+        int greenScore = stateManager.greenScore;
+        int yellowScore = stateManager.yellowScore;
+
+        int[] scores = { blueScore, redScore, greenScore, yellowScore};
+
+        int highestScore = scores.Max();
+
+        if (highestScore == blueScore)
+        {
+            winnerName = ("Blue Player: ");
+        }
+        else if (highestScore == redScore)
+        {
+            winnerName = ("Red Player: ");
+        }
+        else if (highestScore == greenScore)
+        {
+            winnerName = ("Green Player: ");
+        }
+        else if (highestScore == yellowScore)
+        {
+            winnerName = ("Yellow Player: ");
+        }
+
+        firstPlaceText.text = (winnerName + highestScore.ToString());
+    }
+	
+}
