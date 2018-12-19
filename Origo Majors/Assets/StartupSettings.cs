3e0bc7fe23e9259f871c2f14ed97f42367064750
HongKong <pajo0043@gmail.com>
2018-12-19 12:08:46 +0100

    minimun drones is chnaged to 2 now. and is also default number

--- Origo Majors/Assets/StartupSettings.cs
+++ Origo Majors/Assets/StartupSettings.cs
@@ -29,7 +29,7 @@ public class StartupSettings : MonoBehaviour
 
         Debug.Log("Number of drones" + numberOfSelectedDrones);
 
-        numberOfSelectedDrones = dronenumber.value + 4;
+        numberOfSelectedDrones = dronenumber.value + 2;
 
     }
 
