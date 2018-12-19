b50a5dd2ddb52e95820fb31cfba955ee6617a9cf
Christoffer Brandt <43013801+ChristofferBrandtGP18@users.noreply.github.com>
2018-12-19 13:24:24 +0100

    UI

---
+++ Origo Majors/Assets/animController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class animController : MonoBehaviour {
+
+    public StateManager stateManager;
+    public Animator anim;
+    public Player currentPlayer;
+
+
+    void Start () {
+        anim = GetComponent<Animator>();
+	}
+
+	void Update () {
+        if (currentPlayer == Player.Blue)
+        {
+            anim.Play("blueTurn");
+        }
+        else if (currentPlayer == Player.Red)
+        {
+            anim.Play("redTurn");
+        }
+        else if (currentPlayer == Player.Green)
+        {
+            anim.Play("greenTurn");
+        }
+        else if (currentPlayer == Player.Yellow)
+        {
+            anim.Play("yellowTurn");
+        }
+    }
+}
