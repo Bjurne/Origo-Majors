73b5c3384857a0917442d0d8d363771546429e69
Kristoffer Svedlund <kristoffersvedlund@gmail.com>
2019-01-04 14:49:04 +0100

    Prepared UI for avatar images

--- Origo Majors/Assets/animController.cs
+++ Origo Majors/Assets/animController.cs
@@ -1,25 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class animController : MonoBehaviour {
 
     public StateManager stateManager;
     public Player currentPlayer;
 
-    public Animator lastActiveAnimator;
+    private Animator lastActiveAnimator;
     public Animator blueAnimator;
     public Animator redAnimator;
     public Animator greenAnimator;
     public Animator yellowAnimator;
 
     void Start () {
-        blueAnimator.Play("notblueturn");
         redAnimator.Play("notredturn");
+        lastActiveAnimator = redAnimator;
+        StartCoroutine(FadeBackgroundAlpha());
         greenAnimator.Play("notgreenturn");
+        lastActiveAnimator = greenAnimator;
+        StartCoroutine(FadeBackgroundAlpha());
         yellowAnimator.Play("notyellowturn");
-
+        lastActiveAnimator = yellowAnimator;
+        StartCoroutine(FadeBackgroundAlpha());
+        blueAnimator.Play("notblueturn");
         lastActiveAnimator = blueAnimator;
+        StartCoroutine(FadeBackgroundAlpha());
+
+        SetUISize();
 	}
 
 	public void SetUISize () {
@@ -31,25 +41,32 @@ public class animController : MonoBehaviour {
         else if (lastActiveAnimator == greenAnimator) greenAnimator.Play("notgreenturn");
         else if (lastActiveAnimator == yellowAnimator) yellowAnimator.Play("notyellowturn");
 
+        StartCoroutine(FadeBackgroundAlpha());
+        
+
         if (stateManager.currentPlayer == Player.Blue)
         {
             blueAnimator.Play("blueTurn");
             lastActiveAnimator = blueAnimator;
+            StartCoroutine(ResetBackgroundAlpha());
         }
         else if (stateManager.currentPlayer == Player.Red)
         {
             redAnimator.Play("redTurn");
             lastActiveAnimator = redAnimator;
+            StartCoroutine(ResetBackgroundAlpha());
         }
         else if (stateManager.currentPlayer == Player.Green)
         {
             greenAnimator.Play("greenTurn");
             lastActiveAnimator = greenAnimator;
+            StartCoroutine(ResetBackgroundAlpha());
         }
         else if (stateManager.currentPlayer == Player.Yellow)
         {
             yellowAnimator.Play("yellowTurn");
             lastActiveAnimator = yellowAnimator;
+            StartCoroutine(ResetBackgroundAlpha());
         }
 
 
@@ -75,5 +92,37 @@ public class animController : MonoBehaviour {
         //    greenAnimator.Play("notgreenturn");
         //    yellowAnimator.Play("yellowTurn");
         //}
+    }
+
+    public IEnumerator FadeBackgroundAlpha()
+    {
+        lastActiveAnimator.gameObject.GetComponent<RawImage>().CrossFadeAlpha(0, 0.5f, false);
+        lastActiveAnimator.GetComponentInChildren<Image>().CrossFadeAlpha(0, 0.5f, false);
+        yield return new WaitForSeconds(1);
+    }
+
+    public IEnumerator ResetBackgroundAlpha()
+    {
+        if (stateManager.currentPlayer == Player.Blue)
+        {
+            blueAnimator.gameObject.GetComponent<RawImage>().CrossFadeAlpha(1, 0.5f, false);
+            blueAnimator.GetComponentInChildren<Image>().CrossFadeAlpha(1, 0.5f, false);
+        }
+        else if (stateManager.currentPlayer == Player.Red)
+        {
+            redAnimator.gameObject.GetComponent<RawImage>().CrossFadeAlpha(1, 0.5f, false);
+            redAnimator.GetComponentInChildren<Image>().CrossFadeAlpha(1, 0.5f, false);
+        }
+        else if (stateManager.currentPlayer == Player.Green)
+        {
+            greenAnimator.gameObject.GetComponent<RawImage>().CrossFadeAlpha(1, 0.5f, false);
+            greenAnimator.GetComponentInChildren<Image>().CrossFadeAlpha(1, 0.5f, false);
+        }
+        else if (stateManager.currentPlayer == Player.Yellow)
+        {
+            yellowAnimator.gameObject.GetComponent<RawImage>().CrossFadeAlpha(1, 0.5f, false);
+            yellowAnimator.GetComponentInChildren<Image>().CrossFadeAlpha(1, 0.5f, false);
+        }
+        yield return new WaitForSeconds(1);
     }
 }
