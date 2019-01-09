002f9916d467fe10a714a6a5886c792851b2b94f
HongKong <pajo0043@gmail.com>
2019-01-10 00:03:15 +0100

    all drone selection highlights between senactive true/false

--- Origo Majors/Assets/Customization.cs
+++ Origo Majors/Assets/Customization.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Customization : MonoBehaviour {
+public class Customization : MonoBehaviour
+{
 
 
 
@@ -14,37 +15,53 @@ public class Customization : MonoBehaviour {
     public GameObject player3;
     public GameObject player4;
 
-  
+    // buttins for dronebutton reference too keep them highlighted
     public Button player1dronebutton00;
     public Button player1dronebutton01;
     public Button player1dronebutton02;
     public Button player1dronebutton03;
 
+    public Button player2dronebutton00;
+    public Button player2dronebutton01;
+    public Button player2dronebutton02;
+    public Button player2dronebutton03;
+
+    public Button player3dronebutton00;
+    public Button player3dronebutton01;
+    public Button player3dronebutton02;
+    public Button player3dronebutton03;
+
+    public Button player4dronebutton00;
+    public Button player4dronebutton01;
+    public Button player4dronebutton02;
+    public Button player4dronebutton03;
+
     // shows which drone is selected in an int in this script
-    public int player1DroneSelection ;
-    //public int player2DroneSelected = 1;
-    //public int player3DroneSelected = 2;
-    //public int player4DroneSelected = 3;
+    public int player1DroneSelection;
+    public int player2DroneSelection;
+    public int player3DroneSelection;
+    public int player4DroneSelection;
+
+
+
 
 
     public StartupSettings startupsettings;
 
-    // Use this for initialization
-    void Start () {
-        //startupsettings = FindObjectOfType<StartupSettings>();
-
-       
+    void Start()
+    {
     }
-    
-	
-	// Update is called once per frame
-	void Update () {
+
+
+    // Update is called once per frame
+    void Update()
+    {
 
         // check number od selected players 
         // show players 3-4 if their number is selected and if player one is setactive
         numberOfSelectedplayers = playernumber.value + 2;
 
-        if (numberOfSelectedplayers  > 2  && player1.activeInHierarchy == true)
+        if (numberOfSelectedplayers > 2 && player1.activeInHierarchy == true)
         {
             player3.SetActive(true);
         }
@@ -64,7 +81,7 @@ public class Customization : MonoBehaviour {
     }
 
 
-
+    // keep buttons selection always selected, even setactive false/true
     public void Player1buttonpressed()
     {
         player1DroneSelection = startupsettings.player1DroneSelected;
@@ -85,13 +102,77 @@ public class Customization : MonoBehaviour {
         {
             player1dronebutton03.Select();
         }
-
-
-        // this works sorta now, need to be remvoed from update and be run at button click.
     }
+
+
+    public void Player2buttonpressed()
+    {
+        player2DroneSelection = startupsettings.player2DroneSelected;
+
+        if (player2DroneSelection == 0)
+        {
+            player2dronebutton00.Select();
+        }
+        else if (player2DroneSelection == 1)
+        {
+            player2dronebutton01.Select();
+        }
+        else if (player2DroneSelection == 2)
+        {
+            player2dronebutton02.Select();
+        }
+        else if (player2DroneSelection == 3)
+        {
+            player2dronebutton03.Select();
+        }
+    }
+
+
+    public void Player3buttonpressed()
+    {
+        player3DroneSelection = startupsettings.player3DroneSelected;
+
+        if (player3DroneSelection == 0)
+        {
+            player3dronebutton00.Select();
+        }
+        else if (player3DroneSelection == 1)
+        {
+            player3dronebutton01.Select();
+        }
+        else if (player3DroneSelection == 2)
+        {
+            player3dronebutton02.Select();
+        }
+        else if (player3DroneSelection == 3)
+        {
+            player3dronebutton03.Select();
+        }
+    }
+
+    public void Player4buttonpressed()
+    {
+        player4DroneSelection = startupsettings.player4DroneSelected;
+
+        if (player4DroneSelection == 0)
+        {
+            player4dronebutton00.Select();
+        }
+        else if (player4DroneSelection == 1)
+        {
+            player4dronebutton01.Select();
+        }
+        else if (player4DroneSelection == 2)
+        {
+            player4dronebutton02.Select();
+        }
+        else if (player4DroneSelection == 3)
+        {
+            player4dronebutton03.Select();
+        }
+    }
+
 }
 
-
-//numberOfSelectedplayers = playernumber.value + 1;
 
 
